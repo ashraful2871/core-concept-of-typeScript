892bbc381728d366f5795494da35e19fccc52900
ashraful2871 <ashrafulislam2758@gmail.com>
2025-05-01 12:07:05 +0600

    learn reference type in object in typrscript

---
+++ module/src/1.5.ts
@@ -0,0 +1,16 @@
+//Reference Type --. object
+
+const user: {
+  company: "programming Hero"; //type ===> literal Type
+  firstName: string;
+  middleName?: string; //optional Type
+  lastName: string;
+  isMArid: boolean;
+} = {
+  company: "programming Hero",
+  firstName: "Ashraful",
+  lastName: "Islam",
+  isMArid: true,
+};
+
+user.company;
