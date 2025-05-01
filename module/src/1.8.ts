5f233e01f0cc1bc68bb1cb4c826b782821edffc5
ashraful2871 <ashrafulislam2758@gmail.com>
2025-05-01 13:14:06 +0600

    learn how to destructuring object and aray in type script

---
+++ module/src/1.8.ts
@@ -0,0 +1,23 @@
+//destructuring
+{
+  // object destructuring
+
+  const user = {
+    id: 1123,
+    name: {
+      firstName: "Ash",
+      middleNAme: "babu",
+      lastName: "Islam",
+    },
+    contactNo: "01756985231",
+    address: "anderkilla",
+  };
+  const {
+    contactNo,
+    name: { middleNAme },
+  } = user;
+
+  //array destructuring
+  const friends = ["ash", "pro", "rock", "zidan"];
+  const [, , bestFriend, ...friend] = friends;
+}
