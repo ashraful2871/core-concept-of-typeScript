192728bc4ce29545049203678ce047a8b140fa04
ashraful2871 <ashrafulislam2758@gmail.com>
2025-05-01 11:39:45 +0600

    learn basic data type of typeScript

---
+++ module/src/12.1.ts
@@ -0,0 +1,33 @@
+// basic Data type
+
+//  string
+let lastName = "Ash"; //intrusive  Data type
+let firstName: string = "Ash"; //expressive data type
+
+//number
+let roll: number = 123;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+let d: number;
+d = 123;
+
+//none-primitive
+
+//array
+let friends: string[] = ["Ami", "She"];
+
+let Roll: number[] = [1, 2];
+
+//tuple ===> array===> type of value
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string, boolean] = [20, "ash", true];
