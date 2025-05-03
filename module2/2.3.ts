c1e5c88b9313c983b8fc671800503129440d7fc4
ashraful2871 <ashrafulislam2758@gmail.com>
2025-05-03 15:17:50 +0600

    learn generic type in type script

---
+++ module2/2.3.ts
@@ -0,0 +1,44 @@
+{
+  //generic type
+
+  type GenericArray<T> = Array<T>;
+
+  //   const rollNumber: number[] = [3, 5, 6, 98];
+  const rollNumber: GenericArray<number> = [3, 5, 6, 98];
+
+  //   const mentors: string[] = ["ash", "asd", "fgt"];
+  const mentors: GenericArray<string> = ["ash", "asd", "fgt"];
+
+  //   const boolArray: boolean[] = [true, false, true];
+  const boolArray: GenericArray<boolean> = [true, false, true];
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "ash",
+      age: 123,
+    },
+    {
+      name: "pro",
+      age: 103,
+    },
+  ];
+
+  const add = (x: number, y: number) => {
+    return x + y;
+  };
+  add(10, 23);
+
+  //generic tuple
+
+  type genericTuple<x, y> = [x, y];
+  const manus: genericTuple<string, string> = ["mr", "mrs"];
+
+  const userWithId: genericTuple<number, { name: string; email: string }> = [
+    1234,
+    {
+      name: "ash",
+      email: "a@gmail.com",
+    },
+  ];
+  //
+}
